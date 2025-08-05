2ae642588c89d704654c725aa6aa2789dd45e5d2
marcosjrx <marcos@aestufadigital.com.br>
2025-08-04 21:38:50 -0300

    update hero title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <Container>
       <Hero />
       <SectionTitle
-        preTitle="Nextly Benefits"
-        title=" Why should you use this landing page"
+        preTitle="Watts Certo"
+        title=" Economize até 95 % da sua conta de luz ☀️"
       >
         Nextly is a free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
